7df94bcd0eda959e587dd142067c2d71c7036a15
sourabh123456 <sourabhshrm24@gmail.com>
2021-04-18 16:32:54 +0530

    code changes

---
+++ AspNetCoreCrud/AspNetCoreCrud/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace AspNetCoreCrud.Models
+{
+    public partial class Department
+    {
+        public int Id { get; set; }
+        public string DeptName { get; set; }
+        public string DeptCode { get; set; }
+        public DateTime? DateCreated { get; set; }
+    }
+}
---
+++ AspNetCoreCrud/AspNetCoreCrud/Models/EmployeeDBContext.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace AspNetCoreCrud.Models
+{
+    public partial class EmployeeDBContext : DbContext
+    {
+        public EmployeeDBContext()
+        {
+        }
+
+        public EmployeeDBContext(DbContextOptions<EmployeeDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Department> Departments { get; set; }
+        public virtual DbSet<EmpDetail> EmpDetails { get; set; }
+
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=EmployeeDB;User id=so;Password=Nokias1$;");
+//            }
+//        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Department>(entity =>
+            {
+                entity.ToTable("Department");
+
+                entity.Property(e => e.DateCreated).HasColumnType("date");
+
+                entity.Property(e => e.DeptCode)
+                    .IsRequired()
+                    .HasMaxLength(5)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.DeptName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<EmpDetail>(entity =>
+            {
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.EmpCode)
+                    .IsRequired()
+                    .HasMaxLength(5)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.EmpName)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Location)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
